66759660c799eb4614448bbae20baf6b3f290ced
carpentier.ch@gmail.com <carpentier.ch@gmail.com@ff656bfe-f082-4a32-c0ef-6ee59ea73235>
2012-12-11 17:03:11 +0000

    Added a Wedge Mesh and a stub ov the ValueChooser element, akin to those selectable values in console game menus.

---
+++ trunk/src/meshes/Wedge.java
@@ -0,0 +1,164 @@
+package meshes;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer.Type;
+import com.jme3.util.BufferUtils;
+
+public class Wedge extends Mesh
+{
+
+    public enum Orientation
+    {
+
+        LEFT, RIGHT, UP, DOWN
+    };
+
+    public Wedge(Orientation orientation, Vector3f bounds, boolean isExtruded)
+    {
+        updateGeometry(orientation, bounds, isExtruded);
+    }
+
+    /**
+     * Updates the geometry with the given parameters. Bounds are expressed as
+     * if the arrow was pointing to the right, i.e.: x = length of the arrow
+     * (distance to the pointy bit.) y = height of the arrow z = extrusion
+     * length.
+     */
+    public void updateGeometry(Orientation orientation, Vector3f bounds, boolean isExtruded)
+    {
+        // Create three points, according to the size and orientation.
+        Vector3f p0 = new Vector3f(), p1, p2;
+
+        switch (orientation)
+        {
+            case LEFT:
+                p1 = new Vector3f(0, bounds.y, 0);
+                p2 = new Vector3f(-bounds.x, bounds.y / 2, 0);
+                break;
+            case RIGHT:
+                p1 = new Vector3f(bounds.x, bounds.y / 2, 0);
+                p2 = new Vector3f(0, bounds.y, 0);
+                break;
+            case UP:
+                p1 = new Vector3f(bounds.y, 0, 0);
+                p2 = new Vector3f(bounds.y / 2, bounds.x, 0);
+                break;
+            case DOWN:
+                p1 = new Vector3f(bounds.y / 2, -bounds.x, 0);
+                p2 = new Vector3f(bounds.y, 0, 0);
+                break;
+            default:
+                p2 = p1 = p0;
+        }
+
+
+        // Then, make a list of vertices and indices.
+        Vector3f[] vertices, normals;
+        int[] indices;
+        if (!isExtruded)
+        {
+            // For a flat triangle, easy one, just take the initial point and link them.
+            vertices = new Vector3f[3];
+            vertices[0] = p0;
+            vertices[1] = p1;
+            vertices[2] = p2;
+
+            // The normals are all along Z.
+            normals = new Vector3f[3];
+            normals[0] = Vector3f.UNIT_Z;
+            normals[1] = Vector3f.UNIT_Z;
+            normals[2] = Vector3f.UNIT_Z;
+
+            indices = new int[]
+            {
+                0, 1, 2
+            };
+
+        } else
+        {
+            // For an extruded triangle
+            vertices = new Vector3f[18];
+            // First (front) triangle is regular.
+            vertices[0] = p0;
+            vertices[1] = p1;
+            vertices[2] = p2;
+            // Back triangle is easy too: retake first triangle and shift the z-coordinate.
+            vertices[3] = new Vector3f(p0.x, p0.y, -bounds.z);
+            vertices[4] = new Vector3f(p1.x, p1.y, -bounds.z);
+            vertices[5] = new Vector3f(p2.x, p2.y, -bounds.z);
+
+            // Then duplicate side vertices to allow hard edges normals, resulting in 4 quads.
+            vertices[6] = vertices[0];
+            vertices[7] = vertices[3];
+            vertices[8] = vertices[1];
+            vertices[9] = vertices[4];
+
+            vertices[10] = vertices[1];
+            vertices[11] = vertices[4];
+            vertices[12] = vertices[2];
+            vertices[13] = vertices[5];
+
+            vertices[14] = vertices[2];
+            vertices[15] = vertices[5];
+            vertices[16] = vertices[0];
+            vertices[17] = vertices[3];
+
+            // Normals:
+            normals = new Vector3f[18];
+
+            // The front side normal is still the same as for a flat.
+            normals[0] = Vector3f.UNIT_Z;
+            normals[1] = Vector3f.UNIT_Z;
+            normals[2] = Vector3f.UNIT_Z;
+
+            // Backside is the same, but reversed (d'uh).
+            normals[3] = Vector3f.UNIT_Z.negate();
+            normals[4] = normals[3];
+            normals[5] = normals[3];
+
+
+            // Finally, sides normals are computed from the triangle sides. 
+            Vector3f normal1 = new Vector3f(p1.y - p0.y, p0.x - p1.x, 0).normalizeLocal();
+            Vector3f normal2 = new Vector3f(p2.y - p1.y, p1.x - p2.x, 0).normalizeLocal();
+            Vector3f normal3 = new Vector3f(p0.y - p2.y, p2.x - p0.x, 0).normalizeLocal();
+
+            normals[6] = normal1;
+            normals[7] = normal1;
+            normals[8] = normal1;
+            normals[9] = normal1;
+
+            normals[10] = normal2;
+            normals[11] = normal2;
+            normals[12] = normal2;
+            normals[13] = normal2;
+
+            normals[14] = normal3;
+            normals[15] = normal3;
+            normals[16] = normal3;
+            normals[17] = normal3;
+
+            // Finally, the indices.
+            indices = new int[]
+            {
+                // Front
+                0, 1, 2,
+                // Back
+                5, 4, 3,
+                // Sides
+                6, 7, 8, 9, 8, 7,
+                10, 11, 12, 13, 12, 11,
+                14, 15, 16, 17, 16, 15,
+            };
+
+        }
+
+        // Set the mesh data
+        setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
+        setBuffer(Type.Normal, 3, BufferUtils.createFloatBuffer(normals));
+        setBuffer(Type.Index, 3, BufferUtils.createIntBuffer(indices));
+
+        updateBound();
+        setStatic();
+    }
+}
--- trunk/src/test/Main.java
+++ trunk/src/test/Main.java
@@ -9,9 +9,10 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import menu.elements.Button;
 import menu.elements.Carousel;
 import menu.elements.Label;
@@ -19,6 +20,7 @@ import menu.elements.NSlider;
 import menu.elements.Panel;
 import menu.transitions.DirectTransition;
 import menu.utils.FontToMesh;
+import meshes.Wedge;
 
 /**
  * test
@@ -28,10 +30,12 @@ import menu.utils.FontToMesh;
 public class Main extends SimpleApplication
 {
 
+    Geometry geo = null;
     private Panel mainPanel = new Panel();
 
     public static void main(String[] args)
     {
+        Logger.getLogger("").setLevel(Level.OFF);
         Main app = new Main();
         app.start();
     }
@@ -54,7 +58,7 @@ public class Main extends SimpleApplication
         // Attach the menu node.
         rootNode.attachChild(mainPanel);
         mainPanel.setLocalTranslation(0, 0, 0);
-       
+
 
         // Add a title label.
         Label label = new Label(FontToMesh.standardExtrudedFont, "3D Menu Demo");
@@ -73,7 +77,7 @@ public class Main extends SimpleApplication
         // Add a carousel to the right.
         Carousel<Node> carousel = new Carousel(true);
         // Add 3 boxes to the carousel.
-        for (char c: "Carousel".toCharArray())
+        for (char c : "Carousel".toCharArray())
         {
             Node node = new Node();
             Geometry geometry = new Geometry();
@@ -83,14 +87,14 @@ public class Main extends SimpleApplication
             geometry.getLocalTranslation().subtractLocal(FontToMesh.standardExtrudedFont.getGlyphSize(c).mult(0.5f));
             geometry.setMaterial(simpleLightMaterial);
             node.attachChild(geometry);
-            
+
             node.attachChild(node);
             carousel.addElements(node);
         }
         carousel.setLocalTranslation(2f, 0, 0);
         panel1.add(carousel);
-        
-        
+
+
         // Also add a button to trigger a transition.
         Button button = new Button(FontToMesh.standardFlatFont, "Button test!");
         // Move the label on the left of the menu.
@@ -138,6 +142,12 @@ public class Main extends SimpleApplication
             }
         });
 
+        /*Wedge wedge = new Wedge(Wedge.Orientation.RIGHT, new Vector3f(0.3f, 1, 1),true);
+         geo = new Geometry("OurMesh", wedge);
+         geo.setMaterial(simpleLightMaterial);
+         geo.setLocalTranslation(0, -2, 0);
+         mainPanel.attachChild(geo);*/
+
         // Add a sun (on the right)
         DirectionalLight sun = new DirectionalLight();
         sun.setColor(ColorRGBA.White);
@@ -149,23 +159,23 @@ public class Main extends SimpleApplication
         al.setColor(ColorRGBA.White.mult(0.7f));
         rootNode.addLight(al);
     }
-
-
     /**
      * The menu root needs to be updated, so that moving objects can be
      * refreshed.
      */
     float x = 0f;
+
     @Override
     public void simpleUpdate(float tpf)
     {
         super.simpleUpdate(tpf);
         mainPanel.update(tpf);
-        
+
         // To add to the 3D effect, make the menu swing back and forth.
-        x+=tpf;
-        mainPanel.setLocalRotation(new Quaternion().fromAngles(0.2f*FastMath.sin(x), 0.2f*FastMath.cos(x),0));
+        x += tpf;
+        mainPanel.setLocalRotation(new Quaternion().fromAngles(0.2f * FastMath.sin(x), 0.2f * FastMath.cos(x), 0));
         //mainPanel.getLocalRotation().set(0.4f*FastMath.sin(x % FastMath.PI)+1, 0, 0, 1);
 
+        geo.setLocalRotation(new Quaternion().fromAngles(x, 0, 0));
     }
 }
